3a11b6a578ac6d12a463d594e3f6327be0903ef8
ChecoChecoGH <as.ignasi97@gmail.com>
2021-05-04 12:18:36 +0200

    Terminado, falta perfeccionarlo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,12 @@
 // Ej1: Busca a todas las personas que no se llaman Sergio pero que tienen una "s" en su nombre.
 // Ej2: Busca a todas las personas cuyo nombre tiene un número de letras par.
 // Ej3: Busca a todas las personas cuyo nombre tiene un número de letras superior a 6 y que su nombre empieza por vocal.
-// Ej4: Busca a todas las personas cuyo nombre tiene que tengan 3 vocales o más.
-val l1 = mutableListOf("Carlos", "Sergio", "Javier", "Ignacio","Manuel", "Stefanel", "Néstor")
+// Ej4: Busca a todas las personas cuyo nombre tiene que tengan más de 3 vocales distintas.-
+
+val l1 = mutableListOf("Carlos", "Sergio", "Javier", "Ignacio", "Manuel", "Stefanel", "Nestor")
 val listaVocales = mutableListOf('A', 'E', 'I', 'O', 'U')
+const val NUMEROVOCALES = 5-1
+
 fun main() {
     //Ejercicio1
     val resultadoEj1 = l1.filter{ it.toUpperCase().contains('S') && it.toUpperCase() != "SERGIO" }
@@ -21,17 +24,15 @@ fun main() {
     //Ejercicio4
     val resultadoEj4 = l1.filter{ nombre ->
         var contAuxContadores = 0
-        nombre.forEach { letra ->
-            val auxContVocales = mutableListOf(false, false, false, false, false)
-            var i = 0
-            listaVocales.forEach { vocal ->
-                if((letra.toUpperCase() == vocal) && (!auxContVocales[i++])){ auxContVocales[i] = true }
+        val auxNombre = nombre.toUpperCase()
+        val auxContVocales = mutableListOf(false, false, false, false, false)
+        auxNombre.forEach { letra ->
+            for(i in 0..NUMEROVOCALES){
+                if(letra == listaVocales[i]){ auxContVocales[i] = true }
             }
-
-            //auxContVocales.forEach { if(it){ contAuxContadores++ } }
-            //contAuxContadores >= 3
         }
-        contadorVocalesDistintas == 3
+        auxContVocales.forEach { if(it){ contAuxContadores++ } }
+        contAuxContadores >= 3
     }
     println(resultadoEj4)
 }
